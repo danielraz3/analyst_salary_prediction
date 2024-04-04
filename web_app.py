037552834c289ed2ab36c8bb5a27a4621ca4045e
Daniel Raz <danielr@m-danielr-y2qm.playtika.local>
2024-04-05 00:19:16 +0300

    zxzxzc

--- web_app.py
+++ web_app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[27]:
+# In[28]:
 
 
 import streamlit as st
@@ -26,25 +26,32 @@ def main():
         .ltr-slider .stSlider > div {
             direction: ltr;
         }
+        .footer {
+            position: fixed;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            padding: 1rem;
+            background-color: white;
+            text-align: center;
+        }
         </style>
         """,
         unsafe_allow_html=True
     )
     
-    st.title('מחשבון שכר לאנליסטים 2024')
-    # Adding a smaller title under the main title
+    # Main title centered
     st.markdown("""
-    <h2 style='text-align: center; color: gray;'>המחשבון מבוסס על תוצאות סקר אנליסטים שנערך בקבוצת <a href="https://www.facebook.com/groups/DataAnalyticsIsrael" target="_blank">Data Analyst</a></h2>
+    <h1 style='text-align: center;'>מחשבון שכר לאנליסטים 2024</h1>
     """, unsafe_allow_html=True)
     
+    # Smaller, centered subtitle with hyperlink
     st.markdown("""
-    <div class="footer">
-    פותח על ידי <a href="https://www.linkedin.com/in/daniel-raz-1747b2118/" target="_blank">דניאל רז</a>
-    </div>
+    <h2 style='text-align: center; color: gray; font-size: 16px;'>המחשבון מבוסס על תוצאות סקר אנליסטים שנערך בקבוצת <a href="https://www.facebook.com/groups/DataAnalyticsIsrael" target="_blank">Data Analyst</a></h2>
     """, unsafe_allow_html=True)
-    
+
+    # Define the app layout and input widgets
     col1, col2 = st.columns(2)
-    
     with col1:
         is_sql_list = ['כן', 'לא', 'לעיתים נדירות']
         is_sql = st.selectbox("האם התפקיד כולל שימוש באס.קיו.אל?", is_sql_list)
@@ -54,7 +61,6 @@ def main():
         is_python = st.selectbox("האם התפקיד כולל שימוש בשפת פייתון?", is_ml_list)
         
     col3, col4 = st.columns(2)
-
     with col3:
         is_viz_tool = ['Tableau', 'Power BI', 'Excel', 'Looker/Qlik/Python/R', 'Other', 'No Tool']
         viz_tool = st.selectbox("מהו כלי הויזואליזציה העיקרי בו אתה משתמש?", is_viz_tool)
@@ -64,7 +70,6 @@ def main():
         is_manager = st.selectbox("האם תפקיד ניהולי?", is_manager_list)
 
     col5, col6 = st.columns(2)
-    
     with col5:
         company_type_list = ['הייטק', 'תעשייה ישראלית', 'אחר']
         company_type = st.selectbox("סוג החברה", company_type_list)
@@ -75,17 +80,27 @@ def main():
     
     exp = st.slider("שנות נסיון", min_value=0, max_value=20, value=0)
 
-    if st.button("Predict"):
-        data = {'company_type': company_type, 'is_manager': is_manager, 'is_sql': is_sql, 'is_python': is_python,
-                'year_of_surv': '2024', 'exp': exp, 'viz_tool': viz_tool, 'analyst_type': analyst_type}
-        
-        new_input_data = pd.DataFrame([list(data.values())], columns=['company_type', 'is_manager', 'is_sql', 'is_python', 'year_of_surv', 'exp', 'viz_tool', 'analyst_type'])
-        
-        prediction_input = preprocessor.transform(new_input_data)
-        prediction = model.predict(prediction_input)
-        prediction_formatted = f"{int(round(prediction[0], -2)):,}"
-        
-        st.markdown(f"<h2 style='text-align: center; color: black;'>Predicted Salary: {prediction_formatted} ₪</h2>", unsafe_allow_html=True)
+    # Center the "Predict" button
+    col1, col2, col3 = st.columns([1, 2, 1])
+    with col2:
+        if st.button("Predict"):
+            data = {'company_type': company_type, 'is_manager': is_manager, 'is_sql': is_sql, 'is_python': is_python,
+                    'year_of_surv': '2024', 'exp': exp, 'viz_tool': viz_tool, 'analyst_type': analyst_type}
+            
+            new_input_data = pd.DataFrame([list(data.values())], columns=['company_type', 'is_manager', 'is_sql', 'is_python', 'year_of_surv', 'exp', 'viz_tool', 'analyst_type'])
+            
+            prediction_input = preprocessor.transform(new_input_data)
+            prediction = model.predict(prediction_input)
+            prediction_formatted = f"{int(round(prediction[0], -2)):,}"
+            
+            st.markdown(f"<h2 style='text-align: center; color: black;'>Predicted Salary: {prediction_formatted} ₪</h2>", unsafe_allow_html=True)
+
+    # Add footer with link
+    st.markdown("""
+    <div class="footer">
+    פותח על ידי <a href="https://www.linkedin.com/in/daniel-raz-1747b2118/" target="_blank">דניאל רז</a>
+    </div>
+    """, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
